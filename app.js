fe1ae6da6ff6702693cbe55669bba6489294d868
Jorge Aluizio <jaasouza@gmail.com>
2016-08-14 19:02:15 -0300

    Implementando 02.

--- app.js
+++ app.js
@@ -26,11 +26,15 @@ var app = new Vue({
             'Empréstimo',
             'Gasolina'
         ],
-        bills: []
-    },
-
-    ready: function() {
-        this.bills = this.getItems();
+        bills: [
+            {date_due: '20/08/2016', name: 'Conta de luz', value: 70.99, done: 1},
+            {date_due: '21/08/2016', name: 'Conta de água', value: 50.99, done: 1},
+            {date_due: '22/08/2016', name: 'Conta de telefone', value: 55.99, done: 0},
+            {date_due: '23/08/2016', name: 'Supermercado', value: 625.99, done: 0},
+            {date_due: '24/08/2016', name: 'Cartão de crédito', value: 1500.99, done: 1},
+            {date_due: '25/08/2016', name: 'Empréstimo', value: 2000.99, done: 0},
+            {date_due: '26/08/2016', name: 'Gasolina', value: 200, done: 0}
+        ]
     },
 
     computed: {
@@ -46,16 +50,11 @@ var app = new Vue({
             if (!count) {
                 return {tag: -1, value: "Nenhuma conta cadastrada"};
             }
-            return !countDone ? {tag: 0, value: "Nenhuma conta a pagar"} : {tag: 1, value: "Existe(m) " + countDone + " conta(s) a ser(em) paga(s)"};
+            return countDone == 0 ? {tag: 0, value: "Nenhuma conta a pagar"} : {tag: 1, value: "Existe(m) " + countDone + " conta(s) a ser(em) paga(s)"};
         }
     },
 
     methods: {
-        getItems: function() {
-            this.$http.get('items.json', function(data){
-                this.bills = data;
-            });
-        },
         showView: function (id) {
             this.activedView = id;
             this.bill = {
